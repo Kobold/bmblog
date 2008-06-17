218531c00eb5a541d06216411d97f2e9e7a2e8a0
Andy Kish <agkish@gmail.com>
2008-06-17 17:58:09 -0400

    Updated styling and added logo.

--- urls.py
+++ urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('',
     (r'^$', 'posts.views.main'),
     
     (r'^admin/', include('django.contrib.admin.urls')),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/Users/kobold/bmblog/static'}),
 )
