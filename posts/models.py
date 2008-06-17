ca421c6d50c1d0887ba304e57ed05cf69af7b66e
Andy Kish <agkish@gmail.com>
2008-06-16 23:37:50 -0400

    Created post model.

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ["-date"]
