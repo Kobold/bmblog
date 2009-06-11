c053fa312c45631936be1984098159e29df859be
Andy Kish <agkish@gmail.com>
2009-06-11 14:07:37 -0400

    now displays all markdown posts in a given directory

--- blog.py
+++ blog.py
@@ -8,6 +8,8 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 ENVIRONMENT = Environment(
     loader=FileSystemLoader(os.path.join(PROJECT_ROOT, 'templates')))
 
+# in memory list of posts that is loaded at startup
+posts = []
 
 def render_template(name, variables):
     """Renders the template ``name`` using ``variables``."""
@@ -17,14 +19,22 @@ def render_template(name, variables):
 
 class Blog(object):
     def index(self):
-        md = markdown.Markdown(extensions=['meta'])
-        text = file('/Users/kobold/test_data/article.markdown').read()
-        html = md.convert(text)
         return render_template('index.html', {
-            'title': md.Meta['title'][0],
-            'date': md.Meta['date'][0],
-            'post': html,
+            'posts': posts,
         })
     index.exposed = True
+
+# load the posts on startup
+post_directory = os.environ['BMBLOG_POST_DIRECTORY']
+post_files = [f for f in os.listdir(post_directory)
+                if f.endswith('.markdown')]
+for f in post_files:
+    md = markdown.Markdown(extensions=['meta'])
+    html = md.convert(file(os.path.join(post_directory, f)).read())
+    posts.append({
+        'title': md.Meta['title'][0],
+        'date': md.Meta['date'][0],
+        'body': html,
+    })
 
 cherrypy.quickstart(Blog())
