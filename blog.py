c59a18c43bd25b79a4ca9484fa4a121754732876
Andy Kish <agkish@gmail.com>
2009-06-11 16:27:54 -0400

    added individual pages for blog posts plus simplified templates via inheritance

--- blog.py
+++ blog.py
@@ -13,6 +13,7 @@ TEMPLATE_ROOT = os.path.join(PROJECT_ROOT, 'templates')
 
 environment = Environment(loader=FileSystemLoader(TEMPLATE_ROOT))
 posts = [] # in memory list of posts that is loaded at startup
+posts_by_slug = {}
 
 
 def date(value, format='%B %e, %Y'):
@@ -26,6 +27,11 @@ class Blog(object):
     def index(self):
         template = environment.get_template('index.html')
         return template.render(posts=posts)
+    
+    @cherrypy.expose
+    def post(self, slug):
+        template = environment.get_template('post.html')
+        return template.render(post=posts_by_slug[slug])
 
 
 # load the posts on startup
@@ -36,14 +42,18 @@ for f in post_files:
     md = markdown.Markdown(extensions=['meta'])
     text = file(os.path.join(post_directory, f)).read().decode('utf-8')
     html = md.convert(text)
-    posts.append({
+    post = {
         'title': md.Meta['title'][0],
+        'slug': f[:-9], # slice off the .markdown to create the slug
         'date': dateutil.parser.parse(md.Meta['date'][0]),
         'body': html,
-    })
+    }
     
-    # display posts nicely in chronological order
-    posts.sort(key=operator.itemgetter('date'), reverse=True)
+    posts_by_slug[post['slug']] = post
+    posts.append(post)
+
+# display posts nicely in chronological order
+posts.sort(key=operator.itemgetter('date'), reverse=True)
 
 if __name__ == '__main__':
     conf = {
