a4605e1849ee076e5c55b7a614e5194d753f81b8
Andy Kish <agkish@gmail.com>
2009-06-11 13:30:14 -0400

    got Jinja2 going for doing templates

--- blog.py
+++ blog.py
@@ -1,8 +1,24 @@
+# -*- coding: utf-8 -*-
+from jinja2 import Environment, FileSystemLoader
 import cherrypy
+import os
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+ENVIRONMENT = Environment(
+    loader=FileSystemLoader(os.path.join(PROJECT_ROOT, 'templates')))
+
+
+def render_template(name, variables):
+    """Renders the template ``name`` using ``variables``."""
+    template = ENVIRONMENT.get_template(name)
+    return template.render(**variables)
+
 
 class Blog(object):
     def index(self):
-        return "Hello world!"
+        return render_template('index.html', {
+            'phrase': "Hello world!",
+        })
     index.exposed = True
 
 cherrypy.quickstart(Blog())
